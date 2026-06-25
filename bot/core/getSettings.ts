be5efe734550011282f12c7a1365ce8945358439
Bogdan Sandu <bogdanelsandu2011@gmail.com>
2026-06-25 17:07:33 +0300

    remove javascript

--- bot/core/getSettings.ts
+++
@@ -1,5 +0,0 @@
-import * as toml from 'smol-toml';
-import * as fs from 'fs';
-
-const _settingRawString = fs.readFileSync("config.toml", { encoding: 'utf8' });
-export const settings = toml.parse(_settingRawString) as Record<string, any>;
